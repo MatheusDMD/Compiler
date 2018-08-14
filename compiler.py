d68d608915522ae6a6de767fad444e886d06438c
MatheusDMD <matheusdmd@al.insper.edu.br>
2018-08-14 13:25:07 -0300

    OO implementation

--- compiler.py
+++ compiler.py
@@ -1,31 +1,82 @@
+#Compiler version 1.0
+
+#CONSTANTS
+EOF = "EOF"
+PLUS = "PLUS"
+MINUS = "MINUS"
+INT = "INT"
+
+#ERRORS
+operatorError = "A non-digit followed a operator"
+operatorAbsenceError = "A non-operator followed a digit"
+digitAbsenceError = "A non-digit character started the command"
+
+class Token:
+    def __init__(self, value, type):
+        self.value = value
+        self.type = type
+
+class Tokenizer:
+    def __init__(self, origin):
+        self.origin = origin
+        self.position = 0
+        self.current_token = None
+    def selectNextToken(self):
+        value = ""
+        isDigit = False
+        if self.position >= len(self.origin):
+            self.current_token = Token("", EOF)
+            return
+        while self.position < len(self.origin) and self.origin[self.position].isdigit():
+            value += self.origin[self.position]
+            self.position += 1
+            isDigit = True
+        if isDigit:
+            self.current_token = Token(int(value), INT)
+            return
+        if self.origin[self.position] == "+":
+            value = "+"
+            self.position += 1
+            self.current_token = Token(value, PLUS)
+            return
+        if self.origin[self.position] == "-":
+            value = "-"
+            self.position += 1
+            self.current_token = Token(value, MINUS)
+            return
+        raise ValueError(digitAbsenceError)
+        
+class Analyser:
+    tokens = None
+    def init(origin):
+        Analyser.tokens = Tokenizer(origin)
+           
+    def analyseExpression():
+        result = None
+        Analyser.tokens.selectNextToken()
+        if Analyser.tokens.current_token.type == INT:
+            result = Analyser.tokens.current_token.value
+            Analyser.tokens.selectNextToken()
+            while(Analyser.tokens.current_token.type != EOF):
+                if Analyser.tokens.current_token.type == PLUS:
+                    Analyser.tokens.selectNextToken()
+                    if Analyser.tokens.current_token.type == INT:
+                        result += Analyser.tokens.current_token.value
+                    else:
+                        raise ValueError(operatorError)
+                elif Analyser.tokens.current_token.type == MINUS:
+                    Analyser.tokens.selectNextToken()
+                    if Analyser.tokens.current_token.type == INT:
+                        result -= Analyser.tokens.current_token.value
+                    else:
+                        raise ValueError(operatorError)
+                else:
+                    raise ValueError(operatorAbsenceError)
+                Analyser.tokens.selectNextToken()
+        else:
+            raise ValueError(digitAbsenceError)
+        return result
+
 command = (str(input("Calculator: "))).replace(" ", "")
-error = 0
-numbers = []
-operators = []
-number = ""
-for item in command:
-    if item.isdigit():
-        number += item
-    elif item == '+' or item == '-':
-        numbers.append(int(number))
-        operators.append(item)
-        number = ""
-    else:
-        error = 1
-        print("ERRO")
-        break
-
-numbers.append(int(number))
-
-i = 0
-while len(numbers) > 1:
-    a = numbers.pop(0)
-    b = numbers.pop(0)
-    op = operators.pop(0)
-    if op == '+':
-        res = a + b
-    elif op == '-':
-        res = a - b
-    numbers.insert(0, res)
-
-print(numbers[0])
+Analyser.init(command)
+print(Analyser.analyseExpression())
