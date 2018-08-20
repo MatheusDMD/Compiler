ceb2e27740b578f4897c6d4149c5548ff87fc7ef
MatheusDMD <matheusdmd@al.insper.edu.br>
2018-08-20 18:54:29 -0300

    Implementing *,/ and fixing issues

--- compiler.py
+++ compiler.py
@@ -1,15 +1,20 @@
 #Compiler version 1.0
+import re
 
 #CONSTANTS
 EOF = "EOF"
 PLUS = "PLUS"
 MINUS = "MINUS"
 INT = "INT"
+DIVISION = "DIVISION"
+MULTIPLICATION = "MULTIPLICATION"
 
 #ERRORS
 operatorError = "A non-digit followed a operator"
 operatorAbsenceError = "A non-operator followed a digit"
 digitAbsenceError = "A non-digit character started the command"
+commentNotCloseException = "A comment was started but closing symbol '*/' is missing "
+spaceInBetweenDigitsException = "Does not accept space in between digits without operators"
 
 class Token:
     def __init__(self, value, type):
@@ -44,39 +49,87 @@ class Tokenizer:
             self.position += 1
             self.current_token = Token(value, MINUS)
             return
-        raise ValueError(digitAbsenceError)
+        if self.origin[self.position] == "/":
+            value = "/"
+            self.position += 1
+            self.current_token = Token(value, DIVISION)
+            return
+        if self.origin[self.position] == "*":
+            value = "*"
+            self.position += 1
+            self.current_token = Token(value, MULTIPLICATION)
+            return
+        raise Exception(digitAbsenceError)
+
+class PreProcessing:
+    @staticmethod
+    def process(command):
+        return PreProcessing.removeComments(PreProcessing.removeSpaces(command))
         
+
+    @staticmethod
+    def removeSpaces(command):
+        extra_init_comment = re.search('[0-9] +[0-9]', command)
+        if extra_init_comment:
+            raise Exception(spaceInBetweenDigitsException)
+        else:
+            processed_command = command.replace(" ","") #to be improved later
+        return processed_command
+
+    @staticmethod
+    def removeComments(command):
+        processed_command = re.sub('/\*(.*?)\*/', '', command)
+        extra_init_comment = re.search('/\*', processed_command)
+        if extra_init_comment:
+            raise Exception(commentNotCloseException)
+        return processed_command
+
 class Analyser:
     tokens = None
+    @staticmethod
     def init(origin):
         Analyser.tokens = Tokenizer(origin)
-           
-    def analyseExpression():
+
+    @staticmethod
+    def termTreatment():
         result = None
         Analyser.tokens.selectNextToken()
         if Analyser.tokens.current_token.type == INT:
             result = Analyser.tokens.current_token.value
             Analyser.tokens.selectNextToken()
-            while(Analyser.tokens.current_token.type != EOF):
-                if Analyser.tokens.current_token.type == PLUS:
+            while(Analyser.tokens.current_token.type == MULTIPLICATION or Analyser.tokens.current_token.type == DIVISION):
+                if Analyser.tokens.current_token.type == MULTIPLICATION:
                     Analyser.tokens.selectNextToken()
                     if Analyser.tokens.current_token.type == INT:
-                        result += Analyser.tokens.current_token.value
+                        result *= Analyser.tokens.current_token.value
                     else:
-                        raise ValueError(operatorError)
-                elif Analyser.tokens.current_token.type == MINUS:
+                        raise Exception(operatorError)
+                elif Analyser.tokens.current_token.type == DIVISION:
                     Analyser.tokens.selectNextToken()
                     if Analyser.tokens.current_token.type == INT:
-                        result -= Analyser.tokens.current_token.value
+                        result //= Analyser.tokens.current_token.value
                     else:
-                        raise ValueError(operatorError)
+                        raise Exception(operatorError)
                 else:
-                    raise ValueError(operatorAbsenceError)
+                    raise Exception(operatorAbsenceError)
                 Analyser.tokens.selectNextToken()
         else:
-            raise ValueError(digitAbsenceError)
+            raise Exception(digitAbsenceError)
         return result
 
-command = (str(input("Calculator: "))).replace(" ", "")
-Analyser.init(command)
+    @staticmethod     
+    def analyseExpression():
+        result = Analyser.termTreatment()
+        while(Analyser.tokens.current_token.type != EOF):
+            if Analyser.tokens.current_token.type == PLUS:
+                    result += Analyser.termTreatment()
+            elif Analyser.tokens.current_token.type == MINUS:
+                    result -= Analyser.termTreatment()
+            else:
+                raise Exception(operatorAbsenceError)
+        return result
+
+command = (str(input("Calculator: ")))
+processed_command = PreProcessing.process(command)
+Analyser.init(processed_command)
 print(Analyser.analyseExpression())
